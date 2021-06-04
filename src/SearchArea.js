54a3ab8da30412b612a1376b2a4ad1127db150ae
Susan Luebke <78167908+SusanLuebke@users.noreply.github.com>
2021-06-04 18:12:25 +0100

    Ranks videos by Avg User Rating

--- src/SearchArea.js
+++ src/SearchArea.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const SearchArea = (props) => {
   return (
-    <div className="search-area" >
+    <div className="search-area">
       <form onSubmit={props.searchVideo} action="">
-        <input type="text" onChange={props.handleSearch} /> 
+        <input type="text" onChange={props.handleSearch} />
         <button type="submit">Search</button>
-        <select defaultValue='Sort' onChange={props.handleSort}>
-          <option disabled value='Sort'>Sort</option>
-          <option value='Top 20'>Top 20</option>
+        <select defaultValue="Sort" onChange={props.handleSort}>
+          <option disabled value="Sort">
+            Sort
+          </option>
+          <option value="Top 20">Top 20</option>
         </select>
       </form>
     </div>
--- src/Videos.js
+++ src/Videos.js
@@ -1,64 +1,67 @@
-import React, { Component } from "react";
-import SearchArea from "./SearchArea";
-import request from "superagent";
+import React, { Component } from 'react';
+import SearchArea from './SearchArea';
+import request from 'superagent';
 import VideoList from './VideoList';
 
 class Videos extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        videos: [],
-        searchField: "",
-        sort: "" // updates state with the search term
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      searchField: '',
+      sort: '', // updates state with the search term
+    };
+  }
 
   state = {
     videos: [],
-    searchField: "",
+    searchField: '',
   };
 
-  searchVideo = (event) => { // method
+  searchVideo = (event) => {
+    // method
     event.preventDefault();
     request
-      .get("https://lingumi-take-home-test-server.herokuapp.com/videoTutorials")
+      .get('https://lingumi-take-home-test-server.herokuapp.com/videoTutorials')
       .query({ q: this.state.searchField })
       .then((data) => {
         console.log(data);
-        this.setState({ videos: [...data.body]})
+        this.setState({ videos: [...data.body] });
       });
   };
 
-  handleSearch = (event) => { // method
+  handleSearch = (event) => {
+    // method
     console.log(event.target.value); // verifies search term capture
     this.setState({ searchField: event.target.value }); // value is text typed into search
   };
 
   handleSort = (event) => {
-    this.setState({ sort: event.target.value}) 
-  }
+    this.setState({ sort: event.target.value });
+  };
 
-  cleanData = (data) => { // default value for missing data
-      const cleanedData = data.body.map((video) => {
-        // if(video.hasOwnProperty('averageUserRating') === false) {
-        //   video['averageUserRating'] = 0000;
-        // }
-        // else if(video.hasOwnProperty('imageLinks') === false) {
-        //   video['imageLinks'] = { thumbnail: .\images\video.thumbnail.jpg }
-        // }
+  cleanData = (data) => {
+    // default value for missing data
+    const cleanedData = data.body.map((video) => {
+      // if(video.hasOwnProperty('averageUserRating') === false) {
+      //   video['averageUserRating'] = 0000;
+      // }
+      // else if(video.hasOwnProperty('imageLinks') === false) {
+      //   video['imageLinks'] = { thumbnail: .\images\video.thumbnail.jpg }
+      // }
 
-        return video;
-      })
+      return video;
+    });
 
-      return cleanedData;
-  }
+    return cleanedData;
+  };
 
   render() {
-    const sortedVideos = this.state.videos.sort(a => {
-      if(this.state.sort === 'Top 20') {       
-        return parseInt(a.averageUserRating);
+    const sortedVideos = this.state.videos.sort((a, b) => {
+      if (this.state.sort === 'Top 20') {
+        return a.averageUserRating < b.averageUserRating ? 1 : -1;
       }
-    })
+    });
     return (
       <div>
         <SearchArea
